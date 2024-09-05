dc1874994de66446d3df36fa34c911159ced6baa
AidanAcartis <ralaikotonadia@gmail.com>
2024-09-05 23:47:28 +0300

    start social media

---
+++ src/app/page.js
@@ -0,0 +1,15 @@
+import Card from "./components/Cards.js"
+
+export default function Home() {
+  return (
+    <div className="flex mt-4 max-w-4xl mx-auto gap-6">
+      <div className="w-1/3">
+          <Card>test links</Card>
+      </div>
+      <div className="grow">
+        <Card>form here</Card>
+        <Card>first post test</Card>
+      </div>
+    </div>
+  );
+}

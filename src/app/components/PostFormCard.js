d8a46d2cdccabf056a3452a6f495228ca10cbb91
AidanAcartis <ralaikotonadia@gmail.com>
2024-09-08 08:37:00 +0300

    share button

--- src/app/components/PostFormCard.js
+++ src/app/components/PostFormCard.js
@@ -13,7 +13,7 @@ export default function PostFormCard() {
             </div>
             <div className="flex gap-3">
                     <div>
-                        <button>
+                        <button className="flex gap-2">
                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
                                 <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632Z" />
                             </svg>
@@ -21,7 +21,7 @@ export default function PostFormCard() {
                             People</button>
                     </div>
                     <div>
-                        <button>
+                        <button className="flex gap-2">
                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
                                 <path strokeLinecap="round" strokeLinejoin="round" d="M15 10.5a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
                                 <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1 1 15 0Z" />
@@ -30,12 +30,15 @@ export default function PostFormCard() {
                             check in</button>
                     </div>
                     <div>
-                        <button>
+                        <button className="flex gap-2">
                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
                                 <path strokeLinecap="round" strokeLinejoin="round" d="M15.182 15.182a4.5 4.5 0 0 1-6.364 0M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0ZM9.75 9.75c0 .414-.168.75-.375.75S9 10.164 9 9.75 9.168 9 9.375 9s.375.336.375.75Zm-.375 0h.008v.015h-.008V9.75Zm5.625 0c0 .414-.168.75-.375.75s-.375-.336-.375-.75.168-.75.375-.75.375.336.375.75Zm-.375 0h.008v.015h-.008V9.75Z" />
                             </svg>
 
                             Mood</button>
+                    </div>
+                    <div>
+                        <button className="bg-socialBlue text-white px-6 py-2 rounded-md">Share</button>
                     </div>
             </div>
         </Card>

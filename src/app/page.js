e7c0a49d34ceb207f01a7577ff4922b7ab5d0c11
AidanAcartis <ralaikotonadia@gmail.com>
2024-09-09 08:38:16 +0300

    update

--- src/app/page.js
+++ src/app/page.js
@@ -6,10 +6,10 @@ import PostFormCard from "./components/PostFormCard.js";
 export default function Home() {
   return (
     <div className="flex mt-4 max-w-4xl mx-auto gap-6">
-      <div className="w-1/3">
+      <div className="w-3/12">
           <NavigationCard />
       </div>
-      <div className="grow">
+      <div className="w-9/12">
         <PostFormCard />
         <PostCard />
       </div>

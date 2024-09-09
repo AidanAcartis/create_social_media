9a70e7a6c9a9f28eea74f55f79713bc5ef9c1fc8
AidanAcartis <ralaikotonadia@gmail.com>
2024-09-09 08:23:06 +0300

    post

--- src/app/components/PostCard.js
+++ src/app/components/PostCard.js
@@ -13,6 +13,12 @@ export default function PostCard() {
               <p className="text-gray-500 text-sm">2 hours ago</p>
             </div>
           </div>
+          <div>
+            <p className="my-3 text-sm">L'informatique est un domaine vaste et en constante évolution qui touche de nombreux aspects de notre vie quotidienne. Des ordinateurs aux smartphones, en passant par les systèmes embarqués dans les voitures et les appareils électroménagers, l'informatique joue un rôle crucial dans la façon dont nous interagissons avec le monde.</p>
+            <div className="rounded-md overflow-hidden">
+                <img src="https://crawforduniversity.edu.ng/conas/wp-content/uploads/2020/06/computer-science-header-1024x370.jpg" alt="photos"/>
+            </div>
+          </div>
         </Card>
     );
 }
